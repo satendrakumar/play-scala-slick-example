a6e6d0676f748993e6b5b308ee03b6b3c7e767f7
Satendra Kumar <satendrakumar06@gmail.com>
2018-06-23 18:22:46 +0530

    Remove JUnitRunner

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,11 +1,9 @@
 import org.junit.runner._
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import org.specs2.runner._
 import play.api.test.Helpers._
 import play.api.test._
 
-@RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends PlaySpec with GuiceOneAppPerSuite {
 
   "Application" should {
