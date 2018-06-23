5d38e11e179906a72d55235446cb96ba05e17905
Satendra Kumar <satendrakumar06@gmail.com>
2018-06-23 18:22:46 +0530

    Remove JUnitRunner

--- test/BrowserSpec.scala
+++ test/BrowserSpec.scala
@@ -1,13 +1,11 @@
 import org.junit.runner._
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneServerPerTest
-import org.specs2.runner._
 
 /**
   * add your integration spec here.
   * An integration test will fire up a whole play application in a real (or headless) browser
   */
-@RunWith(classOf[JUnitRunner])
 class BrowserSpec extends PlaySpec
   with OneBrowserPerTest
   with GuiceOneServerPerTest
